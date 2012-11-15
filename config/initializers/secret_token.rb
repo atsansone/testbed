c9caba5b77b4b2a88a9c85b875629bf78886d739
Anthony Sansone <asansone@dailycardinal.net>
2012-11-14 20:42:16 -0600

    Test for checkbox setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testbed::Application.config.secret_token = 'e70fc2ff9e4517a4cf37bd3f5f0228278da0b3f66363c689ab7895029aafc77c9c84ba2934515f556b888fb0b1d22c19dca74193e990e7b8c567114ebfcdbddd'
